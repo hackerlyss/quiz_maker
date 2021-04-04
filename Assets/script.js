35feb8930aa132efd7b86886e0de873e7bd12993
Alyssa Concepcion <alyssanconcepcion@gmail.com>
2021-04-03 19:16:31 -0700

    adding my code of a working quiz with questions and answers and control buttons along with a readme

---
+++ Assets/script.js
@@ -0,0 +1,166 @@
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerEl = document.getElementById('question-container');
+let shuffledQ, currentQ ;
+const questionEl = document.getElementById('question');
+const answerButtonsEl = document.getElementById('answer-buttons');
+const quizTimer = document.getElementById('quiz-timer')
+const userScore = 0;
+
+startButton.addEventListener('click', () => {
+    startGame()
+})
+nextButton.addEventListener('click', () => {
+    currentQ++
+    setNextQ()
+})
+
+var questions = [
+    {
+        question: 'What does CSS stand for?',
+        answers: [
+            { text:"Cascading Style Sheet", correct: true},
+        {text:"Cats Stand Straight", correct:false},
+        {text:"Computation Style Sciences", correct: false}
+        ]
+    },
+    {
+        question: 'What does HTML stand for?',
+        answers: [
+            {text:"Horizontal Type Modulation Locale",correct:false},
+         {text:"Happy Typers Make Lotsacash",correct:false},
+          {text:"HyperText Markup Language", correct:true}
+    ]
+        
+    },
+    {
+        question: 'Which of the statements are true?',
+        answers: [
+            {text:"Java is equivalent to JavaScript",correct: false},
+        {text:"JQuery is a third-party API",correct:true},
+        {text:"You can only style your website with CSS",correct:false}
+        ]
+        
+    },
+    {
+        question: 'What are the 3 scopes in JavaScript?',
+        answers: [
+            {text:"Global, local, block",correct:true},
+        {text:"Micro, Macro, Kaleido",correct:false},
+        {text:'Frontal, occupational, ventral',correct:false}
+        ]
+    
+    },
+    {
+        question: 'Which of the following is semantic HTML?',
+        answers: [
+            {text:"&#60; b &#62;",correct:false},
+         {text:"&#60; br &#62;",correct:false},
+         {text:"&#60; header &#62;",correct:true}
+        ]
+    },
+    {
+        question: 'What two things does addEventListener need to function?',
+        answers: [
+            {text:"for-loop and child",correct:false},
+        {text:"event to listen to and an action to execute",correct:true},
+        {text:"a cup of coffe and their favorite podcast",correct:false}
+        ]
+        
+    },
+    {
+        question: 'What does DOM stand for?',
+        answers: [
+            {text:"Doctorate of Machination",correct:false},
+        {text:"Document Object Model",correct:true},
+        {text:"Data Organization Multiplex",correct:false}
+        ]
+    },
+    {
+        question: 'What does API stand for?',
+        answers: [
+            {text:"Application Programming Interface",correct:true},
+        {text:"Artificial Programming Intelligence",correct:false},
+        {text:"Anarchy Potential Introversion",correct: false}
+        ]
+    },
+]
+
+function addScore (element,correct) {
+    if (correct) {
+        userScore++;
+    }
+}
+
+function startGame() {
+  startButton.classList.add('hide');
+  shuffledQ = questions.sort(() => Math.random() -.5);
+  currentQ = 0;
+  questionContainerEl.classList.remove('hide');
+  setNextQ();
+
+}
+
+function setNextQ() {
+    resetState();
+    showQuestion(shuffledQ[currentQ]);
+}
+
+function showQuestion(question) {
+  questionEl.innerText = question.question;
+  question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn');
+      if (answer.correct) {
+          button.dataset.correct = answer.correct;
+      }  
+      button.addEventListener('click', selectAnswer);
+      answerButtonsEl.appendChild(button)
+      })
+}
+
+function resetState() {
+    clearStatusClass(document.body);
+    nextButton.classList.add('hide');
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button,button.dataset.correct)
+        addScore()
+    })
+    if (shuffledQ.length > currentQ +1) {
+        nextButton.classList.remove('hide');
+    } else {
+        startButton.innerText = 'Restart';
+        startButton.classList.remove('hide')
+    }
+ 
+}
+
+
+function setStatusClass(element,correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Code Quiz
+In this application, I have made a quiz of coding-related questions for the user to answer.
+Every time the user answers incorrectly, the background color will indicate red. When the user answers correctly, the background color changes to green.
